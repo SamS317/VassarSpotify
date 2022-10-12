c0df639f24404947378277ec03d6681ebff31388
BuildTools <unconfigured@null.spigotmc.org>
2022-10-12 00:13:50 -0400

    Added View.java file

--- src/Profile.java
+++
@@ -1,7 +0,0 @@
-public class profile {
-}
-
-//this is in the model thing
-//the profile has the fields username and passwords
-//THE LOGIC OF LOGIN SHOULD BE DONE HERE
-//DOES THIS USERNAME AND PASSWORD WORK TOGETHER
--- src/View.java
+++ src/View.java
@@ -1,4 +1,59 @@
+import java.util.Scanner;
+
 public class View {
+    public static void main(String[] args){
+        Boolean is_logged_in;
+        //Initialising variables
+
+        Scanner scanner = new Scanner(System.in);
+        Controller c = new Controller();
+
+        while(true){
+            System.out.print("VASSAR SPOTIFY\n\n\n");
+            System.out.print("Login (l) or create a new profile (c)");
+
+            String choice = scanner.nextLine();
+
+            //Quitting the program
+            if( choice.equalsIgnoreCase("quit") || choice.equalsIgnoreCase("q")){
+                break;
+            }
+
+            //Logging in
+            if( choice.equalsIgnoreCase("l") ){
+                System.out.print("Name: ");
+                String name = scanner.nextLine();
+
+                System.out.print("Password: ");
+                String password = scanner.nextLine();
+
+                if (c.login(name, password) ){
+                    System.out.print("Welcome " + name + "!");
+                }
+            }
+
+            //Creating a new profile
+            if( choice.equalsIgnoreCase("c") ) {
+                //Getting new username
+                System.out.print("Username: ");
+                String username = scanner.nextLine();
+
+                //Getting Password
+                System.out.print("Password: ");
+                String passwordOne = scanner.nextLine();
+                System.out.print("Re-type password: ");
+                String passwordTwo = scanner.nextLine();
+
+                if ( passwordOne.equals(passwordTwo) ){
+                    if (c.createUser(username, passwordOne) ){
+                        System.out.print("Congrats! " + username + " Your account has been created!");
+                    }
+                } else{
+                    System.out.print("Oops, looks like you already have a profile");
+                }
+            }
+        }
+    }
 }
 
 //All print and scanner (input) statements should live here
