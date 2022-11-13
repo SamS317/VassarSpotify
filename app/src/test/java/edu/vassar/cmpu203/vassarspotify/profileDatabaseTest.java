afa4376512cab1c529a85248dcbbcdc784b464b5
BuildTools <unconfigured@null.spigotmc.org>
2022-11-12 23:51:21 -0500

    Added testing for profile database

---
+++ app/src/test/java/edu/vassar/cmpu203/vassarspotify/profileDatabaseTest.java
@@ -0,0 +1,18 @@
+package edu.vassar.cmpu203.vassarspotify;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import edu.vassar.cmpu203.vassarspotify.model.Profile;
+import edu.vassar.cmpu203.vassarspotify.model.ProfileDatabase;
+
+public class profileDatabaseTest {
+    ProfileDatabase pd = new ProfileDatabase();
+
+    @Test
+    void testAddProfile(){
+        Profile p = new Profile("username123","password123");
+        assertTrue( pd.addProfile(p) );
+        assertEquals(pd.getProfiles().get(0), p);
+    }
+}
