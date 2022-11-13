80d8f1dd2cfde819d863a076f59d5b2b9f971ffd
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 00:37:35 -0500

    Added system test for logging in

---
+++ app/src/androidTest/java/edu/vassar/cmpu203/vassarspotify/loginTest.java
@@ -0,0 +1,37 @@
+package edu.vassar.cmpu203.vassarspotify;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import edu.vassar.cmpu203.vassarspotify.controller.MainActivity;
+
+
+@RunWith(AndroidJUnit4.class)
+public class loginTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void usernameAndPasswordTest() {
+        //Checks logging in works given the pre-existing username and password
+        ViewInteraction usernameVI = Espresso.onView(ViewMatchers.withId(R.id.username));
+        usernameVI.perform(ViewActions.typeText("Connor"));
+
+        ViewInteraction passwordVI = Espresso.onView(ViewMatchers.withId(R.id.password));
+        passwordVI.perform(ViewActions.typeText("Genius"));
+
+        Espresso.closeSoftKeyboard();
+
+        ViewInteraction loginButtonVI = Espresso.onView(ViewMatchers.withId(R.id.logInButton));
+        loginButtonVI.perform(ViewActions.click());
+    }
+}
