1baf9e8d2b15f8781c39308293404216ef6ea942
BuildTools <unconfigured@null.spigotmc.org>
2022-11-17 20:11:25 -0500

    Added testGetProfiles for profile database unit testing

--- app/src/test/java/edu/vassar/cmpu203/vassarspotify/ProfileDatabaseTest.java
+++ app/src/test/java/edu/vassar/cmpu203/vassarspotify/ProfileDatabaseTest.java
@@ -3,6 +3,9 @@ package edu.vassar.cmpu203.vassarspotify;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.vassar.cmpu203.vassarspotify.model.Profile;
 import edu.vassar.cmpu203.vassarspotify.model.ProfileDatabase;
 
@@ -13,6 +16,16 @@ public class ProfileDatabaseTest {
     void testAddProfile(){
         Profile p = new Profile("username123","password123");
         assertTrue( pd.addProfile(p) );
-        assertEquals(pd.getProfiles().get(0), p);
+        assertEquals(pd.getProfiles().get(1), p);
+    }
+
+    @Test
+    void testGetProfiles(){
+        Profile Sam = new Profile("Sam","Super-Genius");
+        pd.addProfile(Sam);
+
+        List<Profile> profiles = pd.getProfiles();
+
+        assertEquals(profiles.get(1).getUsername(), "Sam");
     }
 }
