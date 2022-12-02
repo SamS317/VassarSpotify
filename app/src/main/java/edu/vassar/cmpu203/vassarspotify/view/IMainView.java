f3e0dde54a1808ec5e93b513beff531ce4d34351
Sam Shurin <yourgitemail@example.com>
2022-12-01 19:24:16 -0500

    Added buttons to activity_main and set invisibility of them on logIn screen

--- app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/IMainView.java
+++ app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/IMainView.java
@@ -8,4 +8,11 @@ public interface IMainView {
     public View getRootView();
 
     public void displayFragment(Fragment fragment, boolean allowBack, String name);
+    public void ignoreButtons();
+    public void showButtons();
+    interface Listener{
+        void displaySearchFragment();
+        void displayPlayFragment();
+        void displayHomeFragment();
+    }
 }
--- app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/MainView.java
+++ app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/MainView.java
@@ -1,24 +1,65 @@
 package edu.vassar.cmpu203.vassarspotify.view;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import edu.vassar.cmpu203.vassarspotify.controller.MainActivity;
 import edu.vassar.cmpu203.vassarspotify.databinding.ActivityMainBinding;
+import edu.vassar.cmpu203.vassarspotify.databinding.FragmentLoginFragmentBinding;
 
-public class MainView implements IMainView{
+public class MainView extends Fragment implements IMainView{
 
     FragmentManager fmanager;
-    ActivityMainBinding binding;
 
+    ActivityMainBinding binding;
+    Listener listener;
 
     public MainView(FragmentActivity activity){
         this.fmanager = activity.getSupportFragmentManager();
         this.binding = ActivityMainBinding.inflate(activity.getLayoutInflater());
+
     }
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        this.binding = ActivityMainBinding.inflate(inflater);
+        return this.binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.binding.searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainView.this.listener.displaySearchFragment();
+            }
+        });
+        this.binding.homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainView.this.listener.displayHomeFragment();
+            }
+        });
+        this.binding.playButtonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainView.this.listener.displayPlayFragment();
+            }
+        });
+
+    }
+
     @Override
     public View getRootView() {
         return this.binding.getRoot();
@@ -33,5 +74,15 @@ public class MainView implements IMainView{
             ft.addToBackStack(name);
         }
         ft.commit();
+    }
+    public void ignoreButtons(){
+        this.binding.playButtonHome.setVisibility(View.INVISIBLE);
+        this.binding.homeButton.setVisibility(View.INVISIBLE);
+        this.binding.playButtonHome.setVisibility(View.INVISIBLE);
+    }
+    public void showButtons(){
+        this.binding.playButtonHome.setVisibility(View.VISIBLE);
+        this.binding.homeButton.setVisibility(View.VISIBLE);
+        this.binding.playButtonHome.setVisibility(View.VISIBLE);
     }
 }
