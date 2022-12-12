f5c81d2997e5784d2d1ce665bcf14cad5689a63e
connormachado <cmachado@vassar.edu>
2022-12-12 07:27:37 -0500

    Updated IHomeFragment

--- app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/IHomeFragment.java
+++ app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/IHomeFragment.java
@@ -7,12 +7,14 @@ import edu.vassar.cmpu203.vassarspotify.model.Playlist;
 public interface IHomeFragment {
     interface Listener{
         String getUsername();
-        boolean addPlaylist(String s);
-        List<Playlist> getPlaylists();
         String getPlaylistName(Playlist playlist);
+
+        boolean addPlaylist(String s);
+
+        List<Playlist> getPlaylists();
+
         void displayPlaylistFragment(String name);
         void displayHomeFragment();
         void logOut();
-
     }
 }
