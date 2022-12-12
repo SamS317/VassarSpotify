53a016e713900b521092168da41d41d558686cdb
connormachado <cmachado@vassar.edu>
2022-12-12 04:54:36 -0500

    Updated ProfileDatabase unit testing

--- app/src/test/java/edu/vassar/cmpu203/vassarspotify/ProfileDatabaseTest.java
+++ app/src/test/java/edu/vassar/cmpu203/vassarspotify/ProfileDatabaseTest.java
@@ -1,31 +1,32 @@
 package edu.vassar.cmpu203.vassarspotify;
+import edu.vassar.cmpu203.vassarspotify.model.Profile;
+import edu.vassar.cmpu203.vassarspotify.model.ProfileDatabase;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import edu.vassar.cmpu203.vassarspotify.model.Profile;
-import edu.vassar.cmpu203.vassarspotify.model.ProfileDatabase;
 
 public class ProfileDatabaseTest {
     ProfileDatabase pd = new ProfileDatabase();
 
     @Test
     void testAddProfile(){
+        //Tests that the add profile method works and returns the right profile
         Profile p = new Profile("username123","password123");
         assertTrue( pd.addProfile(p) );
-        assertEquals(pd.getProfiles().get(1), p);
+        assertEquals(pd.getProfiles().get(0), p);
     }
 
     @Test
     void testGetProfiles(){
+        //Tests that the getProfiles method works and returns the correct profile
         Profile Sam = new Profile("Sam","Super-Genius");
         pd.addProfile(Sam);
 
         List<Profile> profiles = pd.getProfiles();
 
-        assertEquals(profiles.get(1).toString(), "Sam");
+        assertEquals(profiles.get(0).toString(), "Sam");
     }
 }
