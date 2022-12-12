750a25ef5aa0867b1ed0a9b1b555264296511e1b
connormachado <cmachado@vassar.edu>
2022-12-12 07:17:39 -0500

    Added javadoc to LoginFragment

--- app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/LoginFragment.java
+++ app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/LoginFragment.java
@@ -18,27 +18,39 @@ import edu.vassar.cmpu203.vassarspotify.databinding.FragmentLoginFragmentBinding
 import edu.vassar.cmpu203.vassarspotify.model.Profile;
 
 
+/**
+ * Provides layout for the login fragment that contains the create user and login methods
+ */
 public class LoginFragment extends Fragment implements ILoginFragment {
-
     FragmentLoginFragmentBinding binding;
     Listener listener;
 
-    public LoginFragment(Listener listener){
-        this.listener = listener;
-    }
+
+    public LoginFragment(Listener listener){ this.listener = listener; }
+
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         this.binding = FragmentLoginFragmentBinding.inflate(inflater);
         return this.binding.getRoot();
     }
 
+
+    /**
+     * Provides a listener to the logIn and createUser buttons
+     * Log in checks the passwords with the given username in the database
+     * CreateUser checks if the username is at least 3 characters long
+     * and if the password is at least 6 characters long
+     *
+     * CreateUser then checks if the username you put in is already taken
+     * @param view Current View
+     * @param savedInstanceState Current savedInstanceState
+     */
     @SuppressLint("SetTextI18n")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
 
         this.binding.logInButton.setOnClickListener(view1 -> {
             Editable usernameE = LoginFragment.this.binding.username.getText();
@@ -46,6 +58,8 @@ public class LoginFragment extends Fragment implements ILoginFragment {
 
             LoginFragment.this.listener.LogIn(usernameE.toString(), passwordE.toString(), LoginFragment.this);
         });
+
+
         this.binding.createButton.setOnClickListener(view12 -> {
             boolean takenUsername = true;
             String usernameE = LoginFragment.this.binding.username.getText().toString();
@@ -57,6 +71,7 @@ public class LoginFragment extends Fragment implements ILoginFragment {
                 LoginFragment.this.binding.logInGate.setText("Please input a password greater than 4 characters long");
             }else{
                 //Now check if this username is already in the database
+                //Next implementations should include decrypting straight from the database
                 List<Profile> profiles = LoginFragment.this.listener.getProfilesForCreateUser();
 
                 for (Profile p: profiles){
@@ -65,13 +80,14 @@ public class LoginFragment extends Fragment implements ILoginFragment {
                         takenUsername = false;
                     }
                 }
+
                 if (takenUsername) {
                     LoginFragment.this.listener.CreateUser(usernameE, passwordE, LoginFragment.this);
                 }
             }
-
         });
     }
+
 
     /**
      * Provides a way to show the user if their password didn't match the username
@@ -86,8 +102,8 @@ public class LoginFragment extends Fragment implements ILoginFragment {
             LoginFragment.this.binding.logInGate.setText("Incorrect password or username");
         }
     }
+
+
     @Override
-    public View getRootView() {
-        return this.binding.getRoot();
-    }
+    public View getRootView() { return this.binding.getRoot(); }
 }
