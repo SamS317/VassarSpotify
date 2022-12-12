244d9dd8058067772365317928de29c0ba33867a
connormachado <cmachado@vassar.edu>
2022-12-12 07:12:37 -0500

    Added javadoc for MainView

--- app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/MainView.java
+++ app/src/main/java/edu/vassar/cmpu203/vassarspotify/view/MainView.java
@@ -16,60 +16,46 @@ import edu.vassar.cmpu203.vassarspotify.controller.MainActivity;
 import edu.vassar.cmpu203.vassarspotify.databinding.ActivityMainBinding;
 
 public class MainView extends Fragment implements IMainView{
-
     FragmentManager fmanager;
-
     ActivityMainBinding binding;
     MainActivity mainActivity;
 
+
+    /**
+     * Provides the listeners for the general home, play, and search buttons
+     * @param activity Current activity
+     * @param mainActivity Current MainActivity
+     */
     public MainView(FragmentActivity activity, MainActivity mainActivity){
         this.fmanager = activity.getSupportFragmentManager();
         this.binding = ActivityMainBinding.inflate(activity.getLayoutInflater());
         this.mainActivity = mainActivity;
 
 
-        this.binding.displaySearchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainView.this.mainActivity.displaySearchFragment();
-            }
-        });
+        this.binding.displaySearchButton.setOnClickListener(view -> MainView.this.mainActivity.displaySearchFragment());
 
+        this.binding.homeButton.setOnClickListener(view -> MainView.this.mainActivity.displayHomeFragment());
 
-        this.binding.homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainView.this.mainActivity.displayHomeFragment();
-            }
-        });
-
-
-        this.binding.playButtonHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainView.this.mainActivity.displayPlayFragment();
-            }
-        });
-
+        this.binding.playButtonHome.setOnClickListener(view -> MainView.this.mainActivity.displayPlayFragment());
     }
+
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         this.binding = ActivityMainBinding.inflate(inflater);
         return this.binding.getRoot();
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
     }
+
 
     @Override
-    public View getRootView() {
-        return this.binding.getRoot();
-    }
+    public View getRootView() { return this.binding.getRoot(); }
+
 
     @Override
     public void displayFragment(Fragment fragment, boolean allowBack, String name) {
@@ -81,11 +67,23 @@ public class MainView extends Fragment implements IMainView{
         }
         ft.commit();
     }
+
+
+    /**
+     * Provides a way to hide the main 3 buttons at the bottom of
+     * the screen when we want
+     */
     public void ignoreButtons(){
         this.binding.playButtonHome.setVisibility(View.INVISIBLE);
         this.binding.homeButton.setVisibility(View.INVISIBLE);
         this.binding.displaySearchButton.setVisibility(View.INVISIBLE);
     }
+
+
+    /**
+     * Provides a way to bring back the main 3 buttons at the bottom of
+     * the screen when we want
+     */
     public void showButtons(){
         this.binding.playButtonHome.setVisibility(View.VISIBLE);
         this.binding.homeButton.setVisibility(View.VISIBLE);
